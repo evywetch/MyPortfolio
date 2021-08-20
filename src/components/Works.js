068aa01101041aea89621ba4d3cbd053197936f5
evywetch <evywetch@gmail.com>
2021-08-20 13:20:31 +0200

    Add works images and link with fontawesome

--- src/components/Works.js
+++ src/components/Works.js
@@ -2,6 +2,7 @@ import '../css/Works.css';
 import React from 'react';
 import Link from './Link';
 
+
 class Works extends React.Component {
   render() {
     const animation = true;
@@ -12,8 +13,22 @@ class Works extends React.Component {
             Evy
           </Link>
         </div>
+
         <div className='content'>
-        works
+        <h1>Here are my works...</h1>
+
+        <div className='work'>
+          <p>A Budgety app, built with Javascript, CSS, HTML and module methodology</p>
+        <img src="/images/Budgety.png" alt='Map app'/>
+        <a><i class="far fa-hand-point-right"></i></a>
+        </div>
+
+        <div className='work'>
+          <p>A Mapty app, built with JS(ES6), HTML, CSS, DOM, Geolocation API, Leaflet library and Local Storage.</p>
+        <img src="/images/Mapty.png" alt='Map app'/>
+        <a></a>
+        </div>
+
         </div>
       </div>
     );
@@ -21,3 +36,8 @@ class Works extends React.Component {
 }
 
 export default Works;
+
+/*
+NOTE:🌟🌟🌟
+=> To be able to access fotos with React app, have to add images folder in public folder coz public folder is accessible
+ */
