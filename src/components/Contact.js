1237d549a32e2d27ca11e5582690814febea4bc5
evywetch <evywetch@gmail.com>
2021-08-27 15:02:12 +0200

    Add metaKey and ctrlKey ablility

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,12 @@ import React from 'react';
 import Link from './Link';
 
 class Contact extends React.Component {
+ onClick = (event)=>{
+   if(event.metaKey || event.ctrlKey){
+     return;
+   }
+
+  }
   render() {
     const animation = true;
     return (
@@ -15,7 +21,8 @@ class Contact extends React.Component {
         </div>
 
         <div className="content">
-          Drop me some message at evywetch@gmail.com</div>
+          <h1>Let's get to know each other!</h1>
+          Drop me some message at evywetch@gmail.com or visit my <a href="https://www.linkedin.com/in/evy-wetcharam" onClick={this.onClick}>Linkedin</a></div>
       </div>
     );
   }
