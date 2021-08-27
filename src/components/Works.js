b78b64861b4e62e1c8fe4b953d88a73f3b10fbd7
evywetch <evywetch@gmail.com>
2021-08-27 12:36:04 +0200

    Add Forkify app in Works page

--- src/components/Works.js
+++ src/components/Works.js
@@ -30,6 +30,12 @@ class Works extends React.Component {
         </div>
 
         <div className='work'>
+          <p>A Forkify app</p>
+        <img src="/images/Forkify.png" alt='Forkify app'/>
+        <a class='work-link' href='#'><i class="far fa-hand-point-right"></i> Click here to try it</a>
+        </div>
+
+        <div className='work'>
           <p>A Youtube videos search app, built with React and connect with Youtube API.</p>
         <img src="/images/Videos.png" alt='Videos app'/>
         <a class='work-link' href='#'><i class="far fa-hand-point-right"></i> Click here to try it</a>
@@ -37,10 +43,7 @@ class Works extends React.Component {
 
         <div className='work'>
           <p>A Widgets app.</p>
-          
         <img src="/images/widgets.png" alt='Widgets app'/>
-        
-
         <a class='work-link' href='#'><i class="far fa-hand-point-right"></i> Click here to try it</a>
         </div>
 
@@ -54,5 +57,7 @@ export default Works;
 
 /*
 NOTE:🌟🌟🌟
-=> To be able to access fotos with React app, have to add images folder in public folder coz public folder is accessible
+=> To be able to access fotos with React app, have to add images folder in public folder coz public folder is accessible.
+
+=> Test git stage
  */
