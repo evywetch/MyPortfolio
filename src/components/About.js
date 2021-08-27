876db2e107ddab660861c2f9d5ac271f4d1b9f1f
evywetch <evywetch@gmail.com>
2021-08-27 14:06:18 +0200

    Edit text in About page

--- src/components/About.js
+++ src/components/About.js
@@ -13,14 +13,9 @@ class About extends React.Component {
           </Link>
         </div>
         <div className="content">
-          <h1>I build and make applications work.</h1>
+          <h1>Hey! I am Evy.</h1>
           <p>
-            My name is Evy. I’m a studying front-end developer coming from the
-            backend like Java and other back-end technologies. Now I am learning
-            React to increase my front-end skills after I have just completed a
-            full Javascript course. My goal is to get further training in a
-            place with a teamwork environment. I am enthusiastic, friendly and a
-            real team player.{' '}
+           I am just a frontend student. I can code but but i don't know if i have passion about it. 
           </p>
         </div>
       </div>
