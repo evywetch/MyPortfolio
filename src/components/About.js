74b0e5cce6f655370cb2cc5a3577a9fef786d650
evywetch <evywetch@gmail.com>
2021-09-24 12:12:48 +0200

    Edit About me content

--- src/components/About.js
+++ src/components/About.js
@@ -15,11 +15,10 @@ class About extends React.Component {
         <div className="content">
           <h1>Hey! I am Evy.</h1>
           <p>
-           I am a front-end student. I just code as a hobby. What I like about it is the interaction. 
+           I am a front-end student. I code because it's fun. What I like about it is the interaction. 
            I like the feeling when things work or interact from the code I write. That makes me very excited. 
-           The purpose of this portfolio is just to practice my front-end skills. 
-           I think I like coding but I don't like to work as a developer. I like to be in a talkative and fun environment.
-           So I am not suitable in a developer environment. 
+           The purpose of this portfolio is to practice my Javascript and React skill. 
+         
           </p>
         </div>
       </div>
